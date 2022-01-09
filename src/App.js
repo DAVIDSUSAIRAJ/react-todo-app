cf01067aa2d43481bee41653e8a0a88eb52fcfdd
David <davidsusairaj1996@gmail.com>
2022-01-10 03:59:08 +0530

    add list completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
-
+import { useState } from "react";
 import "./App.scss";
 
 function App() {
+  const [todoname, setTodoname] = useState("");
+  const [todolist, setTodolist] = useState([]);
+  const updateTodoname = (e) => {
+    const value = e.target.value;
+
+    setTodoname(value);
+  };
+
+  const clickAdd = () => {
+    if (todoname != "") {
+      setTodolist([...todolist, { name: todoname, isCompleted: false }]);
+      setTodoname("");
+    }
+  };
+
   return (
     <div className="App">
-      <h1>hello</h1>
+      <div>
+        <h1>ToDo-List</h1>
+        <input
+          type="text"
+          name="text "
+          value={todoname}
+          placeholder="Add your todo value"
+          onChange={updateTodoname}
+        ></input>
+        <button onClick={clickAdd}>Add todo</button>
+        <br></br>
+
+        {todolist.map((item, i) => (
+          <div>
+            <p>
+              {i}
+              {item.name}
+            </p>
+            <span>mark as done</span> &nbsp;
+            <button>Delete</button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/component/Todo.js
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+import "../scss/Todo.scss";
+function Todo(params) {
+  return (
+    <div>
+      <h2>Todo-app</h2>
+      <div className="todo">
+        <p>naem</p>
+        <button>Mark as drop</button>
+        <button>Delete</button>
+      </div>
+    </div>
+  );
+}
+// export default Todo;
