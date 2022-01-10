8bcb2e1ff7a0bf47d13713cac3cda74ccb9634dc
David <davidsusairaj1996@gmail.com>
2022-01-11 00:41:56 +0530

    todo list completed wihout ux

--- src/App.js
+++ src/App.js
@@ -1,21 +1,72 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.scss";
+import Todo from "./component/Todo";
 
 function App() {
   const [todoname, setTodoname] = useState("");
   const [todolist, setTodolist] = useState([]);
+
+  //   TODO INPUT VALUE GET
   const updateTodoname = (e) => {
     const value = e.target.value;
 
     setTodoname(value);
   };
+  // END TODO INPUT VALUE GET
 
+  //START INPUT VALUE PUSH IN ARRAY OF OBJECT
   const clickAdd = () => {
     if (todoname != "") {
       setTodolist([...todolist, { name: todoname, isCompleted: false }]);
       setTodoname("");
     }
   };
+  // END INPUT VALUE PUSH IN ARRAY OF OBJECT
+
+  // LINTTHROUGH STYLE
+
+  const handleMark = (index) => {
+    const newtodolist = [...todolist];
+    console.log(newtodolist[index]);
+
+    if (newtodolist[index].isCompleted) {
+      newtodolist[index].isCompleted = false;
+    } else {
+      newtodolist[index].isCompleted = true;
+    }
+    setTodolist(newtodolist);
+  };
+  // END LINETHROUGH STYLE
+
+  // START DELETE LIST
+
+  const handleDelete = (index) => {
+    const newtodolist = [...todolist];
+    newtodolist.splice(index, 1);
+    setTodolist(newtodolist);
+  };
+
+  // END DELETE LIST
+
+  // 2 USEEFFECT WORK AAGUM,  SECOND ONE BEST PRACTISE;
+  // useEffect(() => {
+  //   window.addEventListener("keydown", handleKeypress);
+  // });
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeypress);
+  }, [clickAdd]);
+
+  // END USEEFFECT TO ENTERKEY
+
+  // KEYPRESSCODE GET AND ONCLICK FUNCTION ADDED
+
+  const handleKeypress = (e) => {
+    //it triggers by pressing the enter key
+    if (e.keyCode === 13) {
+      clickAdd();
+    }
+  };
+  // END KEYPRESSCODE GET AND ONCLICK FUNCTION ADDED
 
   return (
     <div className="App">
@@ -28,7 +79,9 @@ function App() {
           placeholder="Add your todo value"
           onChange={updateTodoname}
         ></input>
-        <button onClick={clickAdd}>Add todo</button>
+        <button onClick={clickAdd} onKeyPress={handleKeypress}>
+          Add todo
+        </button>
         <br></br>
 
         {todolist.map((item, i) => (
@@ -37,11 +90,19 @@ function App() {
               {i}
               {item.name}
             </p>
-            <span>mark as done</span> &nbsp;
-            <button>Delete</button>
+            <span
+              className={item.isCompleted ? "linethrough" : "line"}
+              onClick={() => handleMark(i)}
+            >
+              mark as done
+            </span>{" "}
+            &nbsp;
+            <button onClick={() => handleDelete(i)}>Delete</button>
           </div>
         ))}
       </div>
+
+      <Todo />
     </div>
   );
 }
--- src/component/Todo.js
+++ src/component/Todo.js
@@ -1,16 +1,52 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import "../scss/Todo.scss";
-function Todo(params) {
+
+export default function Todo(params) {
+  const [state, setState] = useState("");
+  const [list, setList] = useState([]);
+  const handleChange = (e) => {
+    setState(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    setList([...list, state]);
+    setState("");
+  };
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeypress);
+  }, [ handleSubmit]);
+  const handleKeypress = (e) => {
+    //it triggers by pressing the enter key
+    if (e.keyCode === 13) {
+      handleSubmit();
+    }
+  };
+
   return (
     <div>
       <h2>Todo-app</h2>
       <div className="todo">
-        <p>naem</p>
+        <p>name</p>
+
+        <input onChange={handleChange} value={state}></input>
+        <button
+          onClick={handleSubmit}
+          type="submit"
+          onKeyPress={handleKeypress}
+        >
+          add
+        </button>
+
+        <br></br>
+        <br></br>
+        {list.map((list) => (
+          <div>
+            <p>{list}</p>
+          </div>
+        ))}
         <button>Mark as drop</button>
         <button>Delete</button>
       </div>
     </div>
   );
 }
-// export default Todo;
